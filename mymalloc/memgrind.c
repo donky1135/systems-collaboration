2e63b20e79f59d8f6d2dc425eeed67cd809380d2
vietnam <17954991+donky1135@users.noreply.github.com>
2024-10-09 21:15:51 -0400

    last minute updates

---
+++ mymalloc/memgrind.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/time.h>
+
+// Compile with -DREALMALLOC to use the real malloc() instead of mymalloc()
+#ifndef REALMALLOC
+#include "mymalloc.h"
+#endif
+
+// Compile with -DLEAK to leak memory
+#ifndef LEAK
+#define LEAK 0
+#endif
+
+
+
+void workload(){
+    for(int i = 0; i < 120; i++){
+        char *test1 = (char*)malloc(1);
+        free(test1);
+    }
+
+    char* test2[120];
+    for(int i = 0; i < 120; i++){
+        test2[i] = (char*)malloc(1);
+    }
+    for(int i = 0; i < 120; i++){
+        free(test2[i]);
+    }
+    // char *test3[120];
+    // char test3Allocated[120];
+    // int allocations = 120;
+    // int numAllocs = 0;
+    // while(allocations){
+    //     int choice = rand() % 2;
+    //     if(choice){
+    //         for(int i = 0; i < 120; i++){
+    //             if(!test3Allocated[i]){
+    //                 test3[i] = (char *)malloc(1);
+    //                 test3Allocated[i]++;
+    //                 allocations--;
+    //                 numAllocs++;
+    //                 break;
+    //             }
+    //         }
+    //     } else {
+    //         if(!numAllocs){
+    //             int temp = 0;
+    //             int choice = rand() % numAllocs;
+    //             for(int i = 0; i < 120; i++){
+    //                 if(test3Allocated[i]){
+    //                     if(temp == choice){
+    //                         free(test3[i]);
+    //                         test3Allocated[i]--;
+    //                         numAllocs--;
+    //                         break;
+    //                     }
+    //                     temp++;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+}
+
+
+int main(){
+    struct timeval start;
+    struct timeval end;
+    gettimeofday(&start, NULL);
+
+    for(int i = 0; i < 50; i++){
+        printf("Workload %02d started", i+1);
+        workload();
+        printf("Workload %02d ended", i+1);
+    }
+    gettimeofday(&end, NULL);
+
+    printf("Average workload time: %d", ((end.tv_sec - start.tv_sec)/50));
+}
--- mymalloc/mymalloc.c
+++ mymalloc/mymalloc.c
@@ -4,11 +4,6 @@
 
 #define DEBUG 1
 
-
-
-
-
-
 #define MEMLENGTH 4096
 #define HEADERSIZE 8
 #define HEADERINT 4
@@ -22,27 +17,18 @@ static union{
 
 
 void heap_print(){
+	printf("\n");
     for(int i = 0; i < MEMLENGTH; i++){
         // if(!i) printf("0000: ");
         printf("%02X ", (heap.bytes[i])&0xff);
         if((i + 1) % 16 == 0 && i+1 != MEMLENGTH)
-            printf("\n");
+            printf("%04d\n", i+1);
     }
-    printf('\n');
+    printf("4096\n");
 }
 
 
 static int is_init = 0;
-
-void init() {
-	int *head_chunk_size = (int *)heap.bytes;
-	int *head_is_allocated = (int *)(heap.bytes + HEADERINT);
-	*head_chunk_size = MEMLENGTH - HEADERSIZE;
-	*head_is_allocated = FREE;
-	is_init = 1;
-	atexit(leak_detector);
-        if(DEBUG) heap_print();
-}
 
 void leak_detector() {
 	int size = 0;
@@ -65,11 +51,27 @@ void leak_detector() {
 	}
 }
 
+
+void init() {
+	int *head_chunk_size = (int *)heap.bytes;
+	int *head_is_allocated = (int *)(heap.bytes + HEADERINT);
+	*head_chunk_size = MEMLENGTH - HEADERSIZE;
+	*head_is_allocated = FREE;
+	is_init = 1;
+	// if(DEBUG) heap_print();
+	atexit(leak_detector);
+	
+}
+
+
 void *mymalloc(size_t size, char *file, int line) {
 	int size_8_mul = (size + 7) & (~7);
 	char *crr;
 
 	if (!(is_init)) init();
+
+	if(size == 0)
+		return NULL;
 
 	crr = heap.bytes;
 
@@ -108,50 +110,73 @@ void myfree(void *ptr, char *file, int line) {
 		fprintf(stderr, "free: Inappropriate pointer (%s:%d)", file, line);
 		exit(2);
 	}
-	
-	while(crr != (heap.bytes + MEMLENGTH)){
+	char *last_free_pointer = NULL;
+	while(crr < (heap.bytes + MEMLENGTH)){
 		int *chunk_size = (int *)crr;
 		int *is_allocated = (int *)(crr + HEADERINT);
 
-		if (is_adjacent(crr, obj) && (*is_allocated == FREE)) { // if the previous chunk free, connect it to the object
-			int *obj_size = (int *)obj;
-			int *obj_is_all = (int *)(obj + HEADERINT);
-
-			if (*obj_is_all == FREE) { // Calling free() a second time on the same pointer
-				fprintf(stderr, "free: Inappropriate pointer (%s:%d)", file, line);
-				exit(2);
-			}
-
-			*chunk_size += (HEADERSIZE + *obj_size);
-			*is_allocated = ALLOCATED; // for the purpose of detecting second call in the next if section
-			obj = crr;
-		}
-
 		if (crr == obj) {
-			char *nxt = crr + (HEADERSIZE + chunk_size);
-			int *nxt_size = (int *)nxt;
-			int *nxt_is_all = (int *)(nxt + HEADERINT);
-
 			if (*is_allocated == FREE) { // Calling free() a second time on the same pointer.
 				fprintf(stderr, "free: Inappropriate pointer (%s:%d)", file, line);
 				exit(2);
 			}
 
-			*is_allocated = FREE;
+			char *nxt = crr + (HEADERSIZE + *chunk_size);
+			int *nxt_size = (int *)nxt;
+			int *nxt_is_all = (int *)(nxt + HEADERINT);
 
-			if ((nxt < (heap.bytes + MEMLENGTH)) && (*nxt_is_all == FREE)) chunk_size += (HEADERSIZE + *nxt_size);
+			if(!is_adjacent(last_free_pointer, obj)){
+				*is_allocated = FREE;
+				if ((nxt < (heap.bytes + MEMLENGTH)) && (*nxt_is_all == FREE)) 
+					*chunk_size += (HEADERSIZE + *nxt_size);
+			} else {
+				*last_free_pointer += *obj + HEADERSIZE;
+				if ((nxt < (heap.bytes + MEMLENGTH)) && (*nxt_is_all == FREE))
+					*last_free_pointer += HEADERSIZE + *nxt_size; 
+			} 
 
 			return;
 		}
+
+		if(*is_allocated == FREE){
+			last_free_pointer = crr;
+		}
+
+		if(obj > HEADERSIZE + crr + 1 && obj < crr + *chunk_size + HEADERSIZE){
+			fprintf(stderr, "free: Inappropriate pointer (%s:%d)", file, line);
+			exit(2);
+		}
+
+		crr += *chunk_size + HEADERSIZE;
+
+		// if (is_adjacent(crr, obj) && (*is_allocated == FREE)) { // if the previous chunk free, connect it to the object
+			
+		// 	int *obj_size = (int *)obj;
+		// 	int *obj_is_all = (int *)(obj + HEADERINT);
+
+		// 	if (*obj_is_all == FREE) { // Calling free() a second time on the same pointer
+		// 		fprintf(stderr, "free: Inappropriate pointer (%s:%d)", file, line);
+		// 		exit(2);
+		// 	}
+
+		// 	*chunk_size += (HEADERSIZE + *obj_size);
+		// 	*is_allocated = ALLOCATED; // for the purpose of detecting second call in the next if section
+		// 	obj = crr;
+		// }
+
+		
 
 		if (crr > obj) { // Calling free() with an address not at the start of a chunk
 			fprintf(stderr, "free: Inappropriate pointer (%s:%d)", file, line);
 			exit(2);
 		}
 	}
+	return;
 }
 
 int is_adjacent(char *crr, char *nxt) {
+	if(crr == NULL)
+		return 0;
 	int *chunk_size = (int *)crr;
 	return ((crr + HEADERSIZE + *chunk_size) == nxt);
 }
